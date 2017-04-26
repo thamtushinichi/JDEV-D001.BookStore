0971440c7cdc20c4318c80239d6e097375125acc
vietlq4 <like.mrtun@gmail.com>
2017-04-26 21:27:54 +0700

    commit user

---
+++ jvd001.BookStore/src/main/java/jvd001/bookstore/app/model/usermanagement/User.java
@@ -0,0 +1,93 @@
+package jvd001.bookstore.app.model.usermanagement;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name="users")
+public class User {
+	@Id
+	@Column(name="users_id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int users_id;
+	private String username;
+	private String password;
+	private String fullname;
+	private int role_id;
+	private String email;
+	private int sex;
+	private int address;
+	private Set<Book> book = new HashSet<Book>(0);
+	
+	
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "users")
+	public Set<Book> getBook() {
+		return book;
+	}
+	public void setBook(Set<Book> book) {
+		this.book = book;
+	}
+	public int getUsers_id() {
+		return users_id;
+	}
+	public void setUsers_id(int users_id) {
+		this.users_id = users_id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	public int getRole_id() {
+		return role_id;
+	}
+	public void setRole_id(int role_id) {
+		this.role_id = role_id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public int getAddress() {
+		return address;
+	}
+	public void setAddress(int address) {
+		this.address = address;
+	}
+	
+
+}

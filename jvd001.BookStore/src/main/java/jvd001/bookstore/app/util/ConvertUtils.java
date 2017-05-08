0a544bd046f8ca643b92c8b5571767bdd7de5262
vietlq4 <like.mrtun@gmail.com>
2017-05-08 20:23:53 +0700

    commit uservo convert

--- jvd001.BookStore/src/main/java/jvd001/bookstore/app/util/ConvertUtils.java
+++ jvd001.BookStore/src/main/java/jvd001/bookstore/app/util/ConvertUtils.java
@@ -4,7 +4,9 @@
 package jvd001.bookstore.app.util;
 
 import jvd001.bookstore.app.dto.BookVO;
+import jvd001.bookstore.app.dto.UserVO;
 import jvd001.bookstore.app.model.bookmanagement.Book;
+import jvd001.bookstore.app.model.usermanagement.*;
 
 /**
  * @author User
@@ -55,4 +57,43 @@ public class ConvertUtils {
 		bookVO.setCategories(book.getCategories());
 		return bookVO;
 	}
+	/**
+	 * Convert UserVOToUse
+	 * @param UserVO
+	 * @return object Use
+	 */
+	public static User convertUserVOToUser(UserVO userVO){
+		User user = new User();
+		user.setUsers_id(userVO.getUsers_id());
+		user.setAddress(userVO.getAddress());
+		user.setEmail(user.getEmail());
+		user.setPassword(userVO.getPassword());
+		user.setFullname(userVO.getFullname());
+		user.setSex(userVO.getSex());
+		user.setUsername(userVO.getUsername());
+		user.setRole_id(userVO.getRole_id());
+		return user;
+				
+	}
+	
+	/**
+	 * Convert UserToUseVO
+	 * @param User
+	 * @return object UseVO
+	 */
+	public static UserVO convertUserToUserVO(User user){
+		UserVO userVO = new UserVO();
+        userVO.setUsers_id(user.getUsers_id());
+		userVO.setAddress(user.getAddress());
+		userVO.setEmail(user.getEmail());
+		userVO.setPassword(user.getPassword());
+		userVO.setFullname(user.getFullname());
+		userVO.setSex(user.getSex());
+		userVO.setUsername(user.getUsername());
+		userVO.setRole_id(user.getRole_id());
+		return userVO;
+		
+	}
+	
+
 }

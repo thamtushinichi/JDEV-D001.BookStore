e9db5a7aec63377f6be2a8de803de9143b1f945f
Nguyễn Tiến Thiên <dualbladev2@gmail.com>
2017-06-01 02:25:28 -0700

    Revert "last comit"
    This reverts commit 5cc447a4ca41b80741943d5b8ef8e863124048d5.

--- jvd001.BookStore/src/main/java/jvd001/bookstore/app/advice/CheckLogin.java
+++ jvd001.BookStore/src/main/java/jvd001/bookstore/app/advice/CheckLogin.java
@@ -1,15 +1,8 @@
 package jvd001.bookstore.app.advice;
 
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-
-@Aspect
 public class CheckLogin {
-	@Before("execution(* jvd001.bookstore.app.controller.bookmanagement.*.*(..))")
-	public void before(JoinPoint joinPoint)
+	public void checkLogin()
 	{
-		System.out.println("done");
+		
 	}
-	
 }
